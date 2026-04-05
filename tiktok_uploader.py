aa3df34da1ff1ca10a787809f28818018e1ebbf9
aminmaskur88 <aminmaskur88@users.noreply.github.com>
2026-04-05 19:11:18 +0700

    fix: restore missing selenium imports

--- tiktok_uploader.py
+++ tiktok_uploader.py
@@ -4,6 +4,13 @@ import time
 import random
 import socket
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException
 
 from selenium.webdriver.common.action_chains import ActionChains
 
